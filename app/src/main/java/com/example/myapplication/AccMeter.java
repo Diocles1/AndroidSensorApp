e706712857c1c5346758cf3e314a994b086be75c
diocles1 <viktor.grasljunga@gmail.com>
2019-04-04 22:26:19 +0200

    Working AccMeter activity

--- app/src/main/java/com/example/myapplication/AccMeter.java
+++ app/src/main/java/com/example/myapplication/AccMeter.java
@@ -1,13 +1,110 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class AccMeter extends AppCompatActivity {
+public class AccMeter extends AppCompatActivity implements SensorEventListener {
+    private TextView txt_x_cord;
+    private TextView txt_y_cord;
+    private TextView txt_z_cord;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mRotationV;
+    private boolean haveSensor = false;
+    private boolean haveSensor2 = false;
+    private float x;
+    private float y;
+    private float z;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acc_meter);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        txt_x_cord = (TextView) findViewById(R.id.x_text);
+        txt_y_cord = (TextView) findViewById(R.id.y_text);
+        txt_z_cord = (TextView) findViewById(R.id.z_text);
+
+        start();
+    }
+
+    private void start() {
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) {
+            if ((mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) || (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)) {
+                noSensorsAlert();
+            }
+            else {
+                mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+            }
+        }
+        else{
+            mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    private void noSensorsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("Your device doesn't support the Compass.")
+                .setCancelable(false)
+                .setNegativeButton("Close",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) {
+            return;
+        }
+        // with t, the low-pass filter's time-constant
+        // and dT, the event delivery rate
+
+        final float alpha = 0.8f;
+
+        float[] gravity = new float[3];
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+        float[] linear_acceleration = new float[3];
+        linear_acceleration[0] = event.values[0] - gravity[0];
+        linear_acceleration[1] = event.values[1] - gravity[1];
+        linear_acceleration[2] = event.values[2] - gravity[2];
+        
+        x = linear_acceleration[0];
+        y = linear_acceleration[1];
+        z = linear_acceleration[2];
+        txt_x_cord.setText("X: " + x);
+        txt_y_cord.setText("Y: " + y);
+        txt_z_cord.setText("Z: " + z);
     }
 }
+
+
