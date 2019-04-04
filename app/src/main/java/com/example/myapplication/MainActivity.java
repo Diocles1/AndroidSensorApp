0cc55cb4b4a58e575274b5c56b4ae30d9430ae5a
diocles1 <viktor.grasljunga@gmail.com>
2019-04-04 11:27:11 +0200

    new compass activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toCompass(View view){
+        Intent intent = new Intent(this, Compass.class);
+        startActivity(intent);
+    }
+    public void toAccMeter(View view){
+        Intent intent = new Intent(this, AccMeter.class);
+        startActivity(intent);
+
     }
 }
